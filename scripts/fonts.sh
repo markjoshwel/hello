1969e52e9dc8ef1049b99857acf38c5a5799b2f1
Mark Joshwel <89562141+markjoshwel@users.noreply.github.com>
2022-04-29 02:56:53 +0000

    Revert moving font files to .fonts/

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,6 +1,5 @@
 # this is a personal script i use to retrieve my fonts
 
 git clone https://github.com/markjoshwel/fonts-private.git
-mkdir -p public/.fonts/
-mv fonts-private/*.woff2 public/.fonts/
+mv fonts-private/*.woff2 public/
 rm -rf fonts-private

d67213b78a6b72d663aca924cef2859b04371ec6
Mark Joshwel <89562141+markjoshwel@users.noreply.github.com>
2022-05-01 03:24:05 +0000

    site: revert mobile viewport hack
    wacky behaviour on desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,6 @@ module.exports = {
         'mono': ['Decima Mono Pro', ...defaultTheme.fontFamily.mono],
       },
     },
-    height: theme => ({
-      auto: 'auto',
-      ...theme('spacing'),
-      full: '100%',
-      screen: 'calc(var(--vh) * 100)',
-    }),
-    minHeight: theme => ({
-      '0': '0',
-      ...theme('spacing'),
-      full: '100%',
-      screen: 'calc(var(--vh) * 100)',
-    }),
   },
   plugins: [],
 }
